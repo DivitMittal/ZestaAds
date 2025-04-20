4a8eeb362140f799480ac47dd8bbb120b966f8f2
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-04-20 06:22:05 +0530

    feat: add nix flake support with treefmt-nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Project Ads flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {flake-parts, ...} @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = builtins.import inputs.systems;
+      imports = [inputs.treefmt-nix.flakeModule];
+      perSystem.treefmt = {
+        flakeCheck = false;
+
+        programs = {
+          #typos.enable = true;
+          ## Nix
+          alejandra.enable = true;
+          deadnix.enable = true;
+          statix.enable = true;
+          ## JSON/HTML
+          prettier.enable = true;
+        };
+
+        projectRootFile = "flake.nix";
+      };
+    };
+}
