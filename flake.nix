5039dc6b2e7c44788f4afa33a490804499a029af
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-05-31 00:29:00 +0530

    chore: cleanup

--- flake.nix
+++ flake.nix
@@ -1,17 +1,10 @@
 {
   description = "Project Ads flake";
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    systems.url = "github:nix-systems/default";
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
 
-  outputs = {flake-parts, ...} @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs = inputs: let
+    inherit (inputs.flake-parts.lib) mkFlake;
+  in
+    mkFlake {inherit inputs;} {
       systems = builtins.import inputs.systems;
       imports = [inputs.treefmt-nix.flakeModule];
       perSystem.treefmt = {
@@ -24,10 +17,17 @@
           alejandra.enable = true;
           deadnix.enable = true;
           statix.enable = true;
-          ## JSON/HTML
-          prettier.enable = true;
         };
-
       };
     };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 }
