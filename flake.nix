9e5744354dacfe989c2ce47f7da7dd732e820a5c
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-07-10 00:08:31 +0530

    feat(nix): initial flake setup
    Sets up a basic flake structure for the project, including
    direnv integration, github actions, pre-commit hooks, and
    devshell.

--- flake.nix
+++ flake.nix
@@ -1,33 +1,42 @@
 {
-  description = "Project Ads flake";
+  description = "{project-name} flake";
 
   outputs = inputs: let
     inherit (inputs.flake-parts.lib) mkFlake;
+    specialArgs.customLib = builtins.import (inputs.OS-nixCfg + "/lib/custom.nix") {inherit (inputs.nixpkgs) lib;};
   in
-    mkFlake {inherit inputs;} {
+    mkFlake {inherit inputs specialArgs;} ({inputs, ...}: {
       systems = builtins.import inputs.systems;
-      imports = [inputs.treefmt-nix.flakeModule];
-      perSystem.treefmt = {
-        flakeCheck = false;
-        projectRootFile = "flake.nix";
-
-        programs = {
-          #typos.enable = true;
-          ## Nix
-          alejandra.enable = true;
-          deadnix.enable = true;
-          statix.enable = true;
-        };
-      };
-    };
+      imports = [./flake];
+    });
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    actions-nix = {
+      url = "github:nialov/actions.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+        pre-commit-hooks.follows = "pre-commit-hooks";
+      };
+    };
+    OS-nixCfg = {
+      url = "github:DivitMittal/OS-nixCfg";
+      flake = false;
     };
   };
 }
